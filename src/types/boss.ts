7d12ae10614d78cf5add22fabfd13b783e7e143f
yonghwankim-dev <dragonbead95@naver.com>
2026-08-13 13:51:23 +0900

    feat - change type, string to Date

--- src/types/boss.ts
+++ src/types/boss.ts
@@ -5,7 +5,7 @@ export interface BossRecord{
   difficulty: string;
   clearTime: string; // 화면 출력용(예: 15분 40초)
   clearTimeSec: number;
-  createdAt: string; // YYYY-MM-DD 형식 (내부 데이터는 시간 포함 가능)
+  createdAt: Date;
 }
 
 export interface Character{
